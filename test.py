4fa8cf16ed2a69e3a1b5ccde929d044e36990838
emilyyyyyyyyyyyyyyyyyyyyyyyyyyyyy <s10244258@connect.np.edu.sg>
2022-09-20 15:15:27 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Hello pig!")
+st.write("Welcome to the one and only website that will deem you as a pig in all languages")
+st.header("Hello babi!")
+st.write("Hello 猪!")
+st.write("Hello 豚!")
+st.write("Hello porca!")
+
+st.header("Thank you for your service, pork")
